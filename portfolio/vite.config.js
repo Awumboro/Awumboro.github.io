43a98c187e214e726cf64a60fdaf5186ab6a3ed6
Justine Kojo <awumboro@gmail.com>
2025-06-07 10:58:31 +0000

    Update vite.config.js

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/justinekojo.github.io/', // 👈 Replace with your GitHub repo name
+    base: '/',
     plugins: [react()],
 });
